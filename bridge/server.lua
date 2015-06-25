8956359800cad6e001b4637933cab1cc6249bda2
Tim Caswell <tim@creationix.com>
2015-06-25 10:24:45 -0500

    Finish bridge

--- bridge/server.lua
+++ bridge/server.lua
@@ -1,21 +1,48 @@
 local createServer = require('coro-net').createServer
 
+
+local logs = {}
+
+
+-- Get ms since Jan. 1 1970 in GMT
+local function gmtNow()
+  local t_secs = os.time() -- get seconds if t was in local time.
+  local t_UTC = os.date("!*t", t_secs) -- find out what UTC t was converted to.
+  return os.time(t_UTC) * 1000 -- find out the converted time in milliseconds.
+end
+
 createServer({
   host = "0.0.0.0",
   port = 11000
 }, function (read, write)
+  local buffer = ""
   for data in read do
-    p(data)
-    write(data)
+    buffer = buffer .. data
+    while true do
+      local line = buffer:match("^[^\n]*\n")
+      if not line then return end
+      if line:match("^metric [^ ]+ [^ ]") then
+        local now = gmtNow()
+        logs[#logs + 1] = "timestamp " .. now .. "\n"
+        logs[#logs + 1] = line
+        write("OK: " .. now .. "\n")
+      else
+        write("Invalid line: " .. line)
+        return write()
+      end
+    end
   end
-  write()
+  return write()
 end)
 print("TCP echo server listening at port 11000 on localhost")
 
-require('fs').unlinkSync("check")
-createServer("check", function (_, write)
-  write("Hello World\n")
+local path = "/var/run/gardener"
+require('fs').unlinkSync(path)
+createServer(path, function (_, write)
+  write("status OK")
+  write(table.concat(logs))
+  logs = {}
   write()
 end)
 
-print("status socket pipe listening at 'check'")
+print("status socket pipe listening at '" .. path .. "'")
